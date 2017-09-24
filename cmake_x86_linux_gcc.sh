ba68ba3b73ca5dc7cc00358b37965be807bcd0b2
kenji ogura <k5i.ogura.v40@gmail.com>
2017-09-24 17:45:05 +0900

    mod

--- cmake_x86_linux_gcc.sh
+++ cmake_x86_linux_gcc.sh
@@ -5,7 +5,7 @@ cmake -G "Unix Makefiles" \
 -DCMAKE_C_COMPILER=gcc \
 -DCMAKE_CXX_COMPILER=g++ \
 -DCMAKE_BUILD_TYPE=RELEASE \
--DCMAKE_INSTALL_PREFIX=/home/kenji/win_shared/cv2413/opencv-2.4.13_x86_Linux_gcc/installed \
+-DCMAKE_INSTALL_PREFIX=/home/kenji/win_shared/cv2413/opencv-2.4.13_x86_linux_gcc/installed \
 -DBUILD_NEW_PYTHON_SUPPORT=OFF \
 -DBUILD_TESTS=OFF \
 -DBUILD_PERF_TESTS=OFF \
