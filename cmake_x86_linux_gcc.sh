54f348bc7f3f9156501adc36691d596d225f4060
kenji ogura <k5i.ogura.v40@gmail.com>
2017-09-24 17:23:06 +0900

    first

---
+++ cmake_x86_linux_gcc.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+export PKG_CONFIG_PATH=/home/kenji/libs/ffmpeg_x86+x264/lib/pkgconfig
+
+cmake -G "Unix Makefiles" \
+-DCMAKE_C_COMPILER=gcc \
+-DCMAKE_CXX_COMPILER=g++ \
+-DCMAKE_BUILD_TYPE=RELEASE \
+-DCMAKE_INSTALL_PREFIX=/home/kenji/win_shared/cv2413/opencv-2.4.13_x86_Linux_gcc/installed \
+-DBUILD_NEW_PYTHON_SUPPORT=OFF \
+-DBUILD_TESTS=OFF \
+-DBUILD_PERF_TESTS=OFF \
+-DWITH_CUDA=OFF \
+-DWITH_OPENCL=OFF \
+-DWITH_GTK=OFF \
+..
